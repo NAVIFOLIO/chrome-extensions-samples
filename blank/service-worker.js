cbd83d5e7a84cc3b22fc29a63219649e293f2055
NAVIFOLIO <programmer@navifolio.jp>
2024-01-30 23:33:45 +0900

    enable sidePanel on the site with URL of 'https://www.google.com'.

--- blank/service-worker.js
+++ blank/service-worker.js
@@ -1,1 +1,21 @@
 console.log("Hello, I'm service worker.");
+
+const GOOGLE_ORIGIN = 'https://www.google.com';
+
+chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+    if (!tab.url) return;
+    const url = new URL(tab.url);
+
+    if (url.origin === GOOGLE_ORIGIN) {
+        await chrome.sidePanel.setOptions({
+            tabId,
+            path: 'sidepanel/sidepanel.html',
+            enabled: true
+        });
+    } else {
+        await chrome.sidePanel.setOptions({
+            tabId,
+            enabled: false
+        });
+    }
+});
