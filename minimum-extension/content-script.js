0340aeedecfc1f9517be8010a379f2528d596a81
NAVIFOLIO <programmer@navifolio.jp>
2024-02-18 10:57:19 +0900

    Change text highlight color in minimum-extension.

--- minimum-extension/content-script.js
+++ minimum-extension/content-script.js
@@ -2,6 +2,6 @@ const titles = document.querySelectorAll('h3');
 
 if (titles){
     for (var i = 0; i < titles.length; i ++){
-        titles[i].style.color = 'yellow';
+        titles[i].style.color = 'red';
     }
 }
